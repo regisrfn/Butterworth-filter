dd48c9d13ffe2817a4913596cb2d56181947f752
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-10-01 12:27:11 -0300

    Ploting filtered signal

--- chebshev/plot_filteredsignal.py
+++ chebshev/plot_filteredsignal.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Importing the dataset
 
-dataset = pd.read_csv('../csv_files/filtro.csv')
+dataset = pd.read_csv('../csv_files/filtro_cheby.csv')
 time = dataset.iloc[:, 0].values
 vout = dataset.iloc[:, 1].values
 vent = dataset.iloc[:, 2].values
