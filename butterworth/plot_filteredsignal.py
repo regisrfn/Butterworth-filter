32ea944518378b93cfcbec776a101f70417dab23
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-10-02 15:02:49 -0300

    PLot signal - butterworth filter

---
+++ butterworth/plot_filteredsignal.py
@@ -0,0 +1,28 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os 
+
+freqs = [1,10,20,30,40]
+
+y_ticks = np.linspace(-0.1,0.1,21)
+fig, ax = plt.subplots()
+for index,f in enumerate(freqs):
+    # Importing the dataset
+    dataset = pd.read_csv(f'../csv_files/butter{index+1}_signal.csv')
+    time = dataset.iloc[:, 0].values
+    vout = dataset.iloc[:, 1].values
+    time = time*1e3
+    ax.plot(time, vout, label=f'{f} kHz')
+
+
+
+ax.set_title('Analise da saida')
+ax.set_ylabel('Tensao (V)')
+ax.set_xlabel('Tempo (ms)')
+# ax.set_xlim(left=0, right=time[-1])
+ax.grid(color='b', ls='-.', lw=0.25)
+plt.yticks(y_ticks)
+plt.legend(loc='upper right')
+plt.show()
--- chebshev/plot_bode.py
+++ chebshev/plot_bode.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('./csv_files/bode_chevy.csv')
+dataset = pd.read_csv('../csv_files/bode_chevy.csv')
 freq = dataset.iloc[:, 0].values
 mag = dataset.iloc[:, 1].values
 
@@ -22,7 +22,6 @@ for N in ordem:
     plt.semilogx(w/(2*np.pi), 20 * np.log10(abs(h)), label=f'Filtro ideal')
 
 
-yticks = np.arange(0,-80,-10)
 plt.title('Resposta em frequência do filtro Chevyshev')
 plt.xlabel('Frequencia [Hz]')
 plt.ylabel('Amplitude [dB]')
@@ -30,7 +29,6 @@ plt.margins(0, 0.1)
 plt.grid(which='both', axis='both')
 plt.axvline(Fn, color='green')  # cutoff frequency
 plt.axhline(-0.5, color='green') # ripple
-plt.legend(loc='upper right')
-# plt.yticks(yticks)
-# plt.ylim(-70,10)
+plt.legend(loc='upper left')
+plt.tight_layout()
 plt.show()
